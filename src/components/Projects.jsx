8704b1b1fae841fd0e8bd8674f8dc7d6b1725408
Shyam <ntshyamsunder96@gmail.com>
2026-06-08 12:24:31 +0530

    update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -26,8 +26,36 @@
 // export default Projects;
 
 import React from 'react';
-import { projectData } from './data/ProjectData';
+import { fullStackProjects, frontendProjects  } from './data/ProjectData';
 import Card from './data/Card';
+
+// const Projects = () => {
+//   return (
+//     <div
+//       className="container-fluid bg-dark bg-image project p-5 text-white"
+//       id="projects"
+//     >
+//       <h2 className="fst-italic fw-bold text-center heading mt-5 pt-5">
+//         Projects
+//       </h2>
+//       <br />
+//       <h3 className="text-center">
+//         I have worked on a wide range of MERN projects. Here are some of my
+//         projects.
+//       </h3>
+//       <div className="row pt-3">
+//         {projectData.map((project, index) => (
+//           <div key={index} className="col-lg-4 col-md-6 col-sm-12 mb-4">
+//             <Card data={project} />
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Projects;
+
 
 const Projects = () => {
   return (
@@ -38,13 +66,31 @@ const Projects = () => {
       <h2 className="fst-italic fw-bold text-center heading mt-5 pt-5">
         Projects
       </h2>
-      <br />
-      <h3 className="text-center">
-        I have worked on a wide range of MERN projects. Here are some of my
-        projects.
+
+      <h3 className="text-center mb-5">
+        I have worked on a variety of Full Stack and Frontend projects.
       </h3>
-      <div className="row pt-3">
-        {projectData.map((project, index) => (
+
+      {/* Full Stack Projects */}
+      <h2 className="text-warning text-center mb-4">
+        Full Stack Projects
+      </h2>
+
+      <div className="row">
+        {fullStackProjects.map((project, index) => (
+          <div key={index} className="col-lg-4 col-md-6 col-sm-12 mb-4">
+            <Card data={project} />
+          </div>
+        ))}
+      </div>
+
+      {/* Frontend Projects */}
+      <h2 className="text-info text-center mt-5 mb-4">
+        Frontend Projects
+      </h2>
+
+      <div className="row">
+        {frontendProjects.map((project, index) => (
           <div key={index} className="col-lg-4 col-md-6 col-sm-12 mb-4">
             <Card data={project} />
           </div>
--- src/components/data/ProjectData.jsx
+++ src/components/data/ProjectData.jsx
@@ -1,60 +1,131 @@
-export const  projectData=[
-    {
-        image:"https://res.cloudinary.com/dn5pbsqio/image/upload/v1721229426/portfolio/hnbrggdbddcdomhftm2m.png",
-        name:"Chat-Sync ",
-        description:"A MERN stack chat application that supports one-on-one and group chats, allowing users to edit group names and manage chat experiences in real-time. ",
-        deployedLink:"https://chat-synce-frontend.netlify.app/",
-        sourceLink:"https://github.com/ShyamSunderNT/Chat-sync-frontend"
-      },
-      {
-        image:"https://res.cloudinary.com/dn5pbsqio/image/upload/v1730012813/portfolio/yqkzraib4fcln18zyvt6.jpg",
-        name:"E Commerce ",
-        description:"This is a fully functional e-commerce application built with React. The app provides a seamless shopping experience, allowing users to register, log in, browse products, manage their accounts, and complete purchases. ",
-        deployedLink:"https://easy-shop-e-commerce.netlify.app/",
-        sourceLink:"https://github.com/ShyamSunderNT/e-commorece-FD"
-      },
-      {
-        image:"https://res.cloudinary.com/dn5pbsqio/image/upload/v1721229427/portfolio/gv6x0ilwjnkycsh98fxw.png",
-        name:"Library Book Management",
-        description:"Library book details Than add book and edit book. ",
-        deployedLink:"https://library-book-management-formik.netlify.app/",
-        sourceLink:"https://github.com/ShyamSunderNT/Library-Book-Management"
-      },
-    {
-      image:"https://res.cloudinary.com/dn5pbsqio/image/upload/v1721229425/portfolio/vozz6fs79fwzmxri2htj.png",
-      name:"To Do ",
-      description:"add task and reminder  ",
-      deployedLink:"https://todo-list-react-ap.netlify.app/",
-      sourceLink:"https://github.com/ShyamSunderNT/todolist"
-    },
+// export const  projectData=[
+//     {
+//         image:"https://res.cloudinary.com/dn5pbsqio/image/upload/v1721229426/portfolio/hnbrggdbddcdomhftm2m.png",
+//         name:"Chat-Sync ",
+//         description:"A MERN stack chat application that supports one-on-one and group chats, allowing users to edit group names and manage chat experiences in real-time. ",
+//         deployedLink:"https://chat-synce-frontend.netlify.app/",
+//         sourceLink:"https://github.com/ShyamSunderNT/Chat-sync-frontend"
+//       },
+//       {
+//         image:"https://res.cloudinary.com/dn5pbsqio/image/upload/v1730012813/portfolio/yqkzraib4fcln18zyvt6.jpg",
+//         name:"E Commerce ",
+//         description:"This is a fully functional e-commerce application built with React. The app provides a seamless shopping experience, allowing users to register, log in, browse products, manage their accounts, and complete purchases. ",
+//         deployedLink:"https://easy-shop-e-commerce.netlify.app/",
+//         sourceLink:"https://github.com/ShyamSunderNT/e-commorece-FD"
+//       },
+//       {
+//         image:"https://res.cloudinary.com/dn5pbsqio/image/upload/v1721229427/portfolio/gv6x0ilwjnkycsh98fxw.png",
+//         name:"Library Book Management",
+//         description:"Library book details Than add book and edit book. ",
+//         deployedLink:"https://library-book-management-formik.netlify.app/",
+//         sourceLink:"https://github.com/ShyamSunderNT/Library-Book-Management"
+//       },
+//     {
+//       image:"https://res.cloudinary.com/dn5pbsqio/image/upload/v1721229425/portfolio/vozz6fs79fwzmxri2htj.png",
+//       name:"To Do ",
+//       description:"add task and reminder  ",
+//       deployedLink:"https://todo-list-react-ap.netlify.app/",
+//       sourceLink:"https://github.com/ShyamSunderNT/todolist"
+//     },
    
-    {
-      image:"https://res.cloudinary.com/dn5pbsqio/image/upload/v1721229426/portfolio/w3otsbzh3zaayjyud3i0.png",
-      name:"Calculator",
-      description:"Simple Calculator using DOM.",
-      deployedLink:"https://cal-culator-dom.netlify.app/",
-      sourceLink:"https://github.com/ShyamSunderNT/calculator-with-Dom"
-    },
-    {
-        image:"https://res.cloudinary.com/dn5pbsqio/image/upload/v1721229426/portfolio/xgn9fjevbexvagatgxlg.png",
-        name:"Emoji Hub",
-        description:"Emoji Hub  fetch 1000 emojis",
-        deployedLink:"https://emoji-cube.netlify.app/",
-        sourceLink:"https://github.com/ShyamSunderNT/Emoji-hub"
-      },
-      {
-        image:"https://res.cloudinary.com/dn5pbsqio/image/upload/v1721229426/portfolio/macpaja4jvf63997qrtr.png",
-        name:"E Commerce ",
-        description:"Product add card and remove cart ",
-        deployedLink:"https://price-cart-site.netlify.app/?",
-        sourceLink:"https://github.com/ShyamSunderNT/PricecartTask"
-      },
-      {
-        image:"https://res.cloudinary.com/dn5pbsqio/image/upload/v1735896741/Screenshot_2025-01-03_143831_n6ycx1.png",
-        name:"IMDB Clone ",
-        description:"An IMDb clone is a movie database platform that provides detailed information about films, TV shows, actors, and reviews, similar to IMDb. review, and track content, while offering personalized recommendations and trending lists.",
-        deployedLink:"https://imdb-clone-shyam.netlify.app/",
-        sourceLink:"https://github.com/ShyamSunderNT/IMDBFD"
-      },
+//     {
+//       image:"https://res.cloudinary.com/dn5pbsqio/image/upload/v1721229426/portfolio/w3otsbzh3zaayjyud3i0.png",
+//       name:"Calculator",
+//       description:"Simple Calculator using DOM.",
+//       deployedLink:"https://cal-culator-dom.netlify.app/",
+//       sourceLink:"https://github.com/ShyamSunderNT/calculator-with-Dom"
+//     },
+//     {
+//         image:"https://res.cloudinary.com/dn5pbsqio/image/upload/v1721229426/portfolio/xgn9fjevbexvagatgxlg.png",
+//         name:"Emoji Hub",
+//         description:"Emoji Hub  fetch 1000 emojis",
+//         deployedLink:"https://emoji-cube.netlify.app/",
+//         sourceLink:"https://github.com/ShyamSunderNT/Emoji-hub"
+//       },
+//       {
+//         image:"https://res.cloudinary.com/dn5pbsqio/image/upload/v1721229426/portfolio/macpaja4jvf63997qrtr.png",
+//         name:"E Commerce ",
+//         description:"Product add card and remove cart ",
+//         deployedLink:"https://price-cart-site.netlify.app/?",
+//         sourceLink:"https://github.com/ShyamSunderNT/PricecartTask"
+//       },
+//       {
+//         image:"https://res.cloudinary.com/dn5pbsqio/image/upload/v1735896741/Screenshot_2025-01-03_143831_n6ycx1.png",
+//         name:"IMDB Clone ",
+//         description:"An IMDb clone is a movie database platform that provides detailed information about films, TV shows, actors, and reviews, similar to IMDb. review, and track content, while offering personalized recommendations and trending lists.",
+//         deployedLink:"https://imdb-clone-shyam.netlify.app/",
+//         sourceLink:"https://github.com/ShyamSunderNT/IMDBFD"
+//       },
 
-  ]
+//   ]
+
+
+export const fullStackProjects = [
+    {
+    image: "https://res.cloudinary.com/dn5pbsqio/image/upload/v1780898951/Snaks_qpvpbt.jpg",
+    name: "snacks-sprint",
+    description: "Developed a MERN stack food ordering platform featuring user registration, login, cart functionality, quantity management, checkout, and order placement. The application provides a smooth and responsive.",
+    deployedLink: "https://snacks-sprint.netlify.app/",
+    sourceLink: "https://github.com/ShyamSunderNT/FD-frontend"
+  },
+
+  {
+    image: "https://res.cloudinary.com/dn5pbsqio/image/upload/v1721229426/portfolio/hnbrggdbddcdomhftm2m.png",
+    name: "Chat-Sync",
+    description: "A MERN stack chat application that supports one-on-one and group chats.",
+    deployedLink: "https://chat-synce-frontend.netlify.app/",
+    sourceLink: "https://github.com/ShyamSunderNT/Chat-sync-frontend"
+  },
+  {
+    image: "https://res.cloudinary.com/dn5pbsqio/image/upload/v1730012813/portfolio/yqkzraib4fcln18zyvt6.jpg",
+    name: "E Commerce",
+    description: "Fully functional e-commerce application built with React and backend integration.",
+    deployedLink: "https://easy-shop-e-commerce.netlify.app/",
+    sourceLink: "https://github.com/ShyamSunderNT/e-commorece-FD"
+  }
+];
+
+export const frontendProjects = [
+  {
+    image: "https://res.cloudinary.com/dn5pbsqio/image/upload/v1721229427/portfolio/gv6x0ilwjnkycsh98fxw.png",
+    name: "Library Book Management",
+    description: "Library book details, add and edit books.",
+    deployedLink: "https://library-book-management-formik.netlify.app/",
+    sourceLink: "https://github.com/ShyamSunderNT/Library-Book-Management"
+  },
+  {
+    image: "https://res.cloudinary.com/dn5pbsqio/image/upload/v1721229425/portfolio/vozz6fs79fwzmxri2htj.png",
+    name: "To Do",
+    description: "Add tasks and reminders.",
+    deployedLink: "https://todo-list-react-ap.netlify.app/",
+    sourceLink: "https://github.com/ShyamSunderNT/todolist"
+  },
+  {
+    image: "https://res.cloudinary.com/dn5pbsqio/image/upload/v1721229426/portfolio/w3otsbzh3zaayjyud3i0.png",
+    name: "Calculator",
+    description: "Simple Calculator using DOM.",
+    deployedLink: "https://cal-culator-dom.netlify.app/",
+    sourceLink: "https://github.com/ShyamSunderNT/calculator-with-Dom"
+  },
+  {
+    image: "https://res.cloudinary.com/dn5pbsqio/image/upload/v1721229426/portfolio/xgn9fjevbexvagatgxlg.png",
+    name: "Emoji Hub",
+    description: "Fetch and display 1000+ emojis.",
+    deployedLink: "https://emoji-cube.netlify.app/",
+    sourceLink: "https://github.com/ShyamSunderNT/Emoji-hub"
+  },
+  {
+    image: "https://res.cloudinary.com/dn5pbsqio/image/upload/v1721229426/portfolio/macpaja4jvf63997qrtr.png",
+    name: "Price Cart",
+    description: "Add products to cart and remove from cart.",
+    deployedLink: "https://price-cart-site.netlify.app/",
+    sourceLink: "https://github.com/ShyamSunderNT/PricecartTask"
+  },
+  {
+    image: "https://res.cloudinary.com/dn5pbsqio/image/upload/v1735896741/Screenshot_2025-01-03_143831_n6ycx1.png",
+    name: "IMDB Clone",
+    description: "Movie database platform with reviews and recommendations.",
+    deployedLink: "https://imdb-clone-shyam.netlify.app/",
+    sourceLink: "https://github.com/ShyamSunderNT/IMDBFD"
+  }
+];
